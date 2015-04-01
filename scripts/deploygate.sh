cdaf9a4227f863d714bdde93a8e9df3a05c8102d
mollifier <mollifier@gmail.com>
2015-04-01 12:46:41 +0900

    upload-ipa-to-deploygate.sh

--- scripts/deploygate.sh
+++ scripts/deploygate.sh
@@ -15,8 +15,6 @@ echo "*     Signing      *"
 echo "********************"
 xcrun -log -sdk iphoneos PackageApplication "$APP_DIR/$APPNAME.app" -o "$APP_DIR/$APPNAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
 
-
 # deploy
-RELEASE_NOTES="Build: ${CIRCLE_BUILD_NUM}, Uploaded: $RELEASE_DATE"
-curl -F file="@$APP_DIR/$APPNAME.ipa"  -F "token=${DEPLOYGATE_API_TOKEN}" -F message="$RELEASE_NOTES" "https://deploygate.com/api/users/${DEPLOYGATE_USER_NAME}/apps"
+./scripts/upload-ipa-to-deploygate.sh -u "$DEPLOYGATE_USER_NAME" -t "$DEPLOYGATE_API_TOKEN" -m "Build: ${CIRCLE_BUILD_NUM}, Uploaded: $RELEASE_DATE" "$APP_DIR/$APPNAME.ipa"
 
