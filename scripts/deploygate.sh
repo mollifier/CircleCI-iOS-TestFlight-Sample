32b2349bac33624a9d05b523f65793df53b12bbb
mollifier <mollifier@gmail.com>
2015-04-01 12:14:58 +0900

    fix

--- scripts/deploygate.sh
+++ scripts/deploygate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_UUID.mobileprovision"
 RELEASE_DATE=$(date '+%Y-%m-%d %H:%M:%S')
 ARCHIVE_PATH="$PWD/build.xcarchive"
 APP_DIR="$ARCHIVE_PATH/Products/Applications"
@@ -7,12 +8,13 @@ APP_DIR="$ARCHIVE_PATH/Products/Applications"
 echo "********************"
 echo "*     Archive      *"
 echo "********************"
-xcodebuild -scheme "$XCODE_SCHEME" -workspace "$XCODE_WORKSPACE" -archivePath "$ARCHIVE_PATH" clean archive -configuration Release
+xcodebuild -scheme "$XCODE_SCHEME" -workspace "$XCODE_WORKSPACE" -archivePath "$ARCHIVE_PATH" clean archive -configuration Release CODE_SIGN_IDENTITY="$DEVELOPER_NAME"
 
 echo "********************"
 echo "*     Signing      *"
 echo "********************"
-xcrun -sdk iphoneos PackageApplication -v  "$APP_DIR/$APPNAME.app" -o "$APP_DIR/$APPNAME.ipa"
+xcrun -log -sdk iphoneos PackageApplication "$APP_DIR/$APPNAME.app" -o "$APP_DIR/$APPNAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+
 
 # deploy
 RELEASE_NOTES="Build: ${CIRCLE_BUILD_NUM}, Uploaded: $RELEASE_DATE"
